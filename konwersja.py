a3a5a34f096103dcfadb623f9d171ac97b974028
Michał <33382785+TiggerGalaxy@users.noreply.github.com>
2020-01-15 12:41:56 +0100

    Add files via upload

---
+++ konwersja.py
@@ -0,0 +1,37 @@
+import math
+
+a = "8.8"
+a = float(a)
+print(type(a))
+print(a)
+
+b = 8.8
+b = int(b)
+print(type(b))
+print(b)
+
+c = "8.8"
+c = float(c)
+c = int(c)
+print(type(c))
+print(c)
+
+d = 8.8
+d = str(d)
+print(type(d))
+print(d)
+
+e = 8
+e = str(e)
+print(type(e))
+print(e)
+
+f = 8
+f = float(f)
+print(type(f))
+print(f)
+
+g = 8
+g = bool(g)
+print(type(g))
+print(g)
